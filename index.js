d4efe1fc12e502b85f46df1a4a46fd500445f237
talehhuseynli <taleh.hsynl@gmail.com>
2024-11-10 23:05:10 +0400

    final update

---
+++ index.js
@@ -0,0 +1,61 @@
+//! Part 1 - String Methods
+//* Task-1
+// let pet = "golden retriever";
+// let result = pet.toLocaleUpperCase(); //hamisi boyuk herif ucun
+
+// console.log(result);
+
+//* Task-2
+// let question = "Can you solve this problem?";
+// let result = question.length;  //uzunlugu olcur
+
+// console.log(result);
+
+//* Task-3
+// let activity = "This game is boring";
+// let result = activity.replace("boring", "exciting");  // istediyimiz sozu evez elemek ucun
+
+// console.log(result);
+
+//* Task-4
+// let headline = "Choose Your Adventure!";
+// let result = headline.split(" "); //array e cevirdik
+// let result2 = result.pop(); // sonuncu indeksi kesdik
+
+// console.log(result2);
+
+//! Part 2 - Array Methods
+//* Task-1
+// let desserts = ["Ice Cream", "Pie", "Pudding"];
+// let newDessert= desserts.push("Cake");  // sona element elave etmek
+
+// console.log(desserts);
+
+//* Task-2
+// let cities = ["London", "New York", "Tokyo"];
+// let search = cities.includes("Paris"); // elemente gore axtaris
+
+// console.log(search);
+
+//* Task-3
+// let cars = ["Sedan", "Coupe"];
+// let trucks = ["Pickup", "SUV"];
+// let mix = cars.concat(trucks); //iki array i birlesdirmek ucun
+
+// console.log(mix);
+
+//* Task-4
+// let desserts = ["Ice Cream", "Pie", "Pudding", "Cake"];
+// let search = desserts.indexOf("Pie");  // index nomreye gore axtaris
+
+// console.log(search);
+
+//! Bonus Task
+let word = "Technology";
+let array = word.split(""); // array e cevirdik
+let result = array.reverse(); // tersine cevirdik
+let string = result.join(""); // string e qaytardiq
+
+console.log(string);
+
+//^   Bezilerini defterimdeki qeydlere baxaraq yazmisam )
